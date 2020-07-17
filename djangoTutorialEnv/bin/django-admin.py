a84e93a51c6cb285dd6cc21dba049dd3678d0d00
Pranilak <aneeshprabu@gmail.com>
2020-07-17 13:29:03 +0530

    My Django Blog, first commit

---
+++ djangoTutorialEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/aneeshprabu/Django Projects/DjangoTutorials/djangoTutorialEnv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
